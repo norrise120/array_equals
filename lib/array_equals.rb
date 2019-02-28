160f01c8897965433c6052f4c17cb09d242fc379
Erica Norris <norrise120@gmail.com>
2019-02-27 21:40:32 -0800

    Created method that compares two arrays by index

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,5 +1,26 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  raise NotImplementedError
+  if array1 == nil || array2 == nil
+    if array1 == nil && array2 == nil
+      return true
+    else
+      return false
+    end
+  elsif array1.length == array2.length
+    if array1.length == 0
+      return true
+    else
+      i = 0
+      array1.length.times do |i|
+        if array1[i] != array2[i]
+          return false
+        end
+        i += 1
+      end
+      return true
+    end
+  else
+    return false
+  end
 end
